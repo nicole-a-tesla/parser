55b8f57d8a63833c69e3cc192a4fb2863347e8fb
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-17 12:25:29 -0500

    cleanup

--- page_parser.rb
+++ page_parser.rb
@@ -6,7 +6,6 @@ class PageParser
   attr_reader :rows, :doc
 
   def initialize(string)
-    # @doc = Nokogiri::HTML(open(url))
     @doc = Nokogiri::HTML(string)
     @rows = get_rows_array(@doc)
   end
@@ -16,21 +15,5 @@ class PageParser
     table_rows = children_of_tbody.each_slice(3).to_a
     table_rows
   end
-
-  def parse_rows(doc)
-
-    table_rows.each do |song_row|
-      # TableRowParser.new(song_row)
-    end
-
- # tr.even/odd has the title and url
- # element 2 (no css) has the tags
- # elem 3 has the artist info
-    rows_tagged_even = doc.css("tr.even")
-    rows_tagged_odd = doc.css("tr.odd")
-    rows_tagged_odd + rows_tagged_even
-
-  end
-
 
 end
--- song_info.rb
+++ song_info.rb
@@ -1,9 +1,11 @@
 class SongInfo
   attr_reader :title, :url, :artists, :tags
+
   def initialize(info)
     @title = info[:title]
     @url = info[:url]
     @artists = info[:artists]
     @tags = info[:tags]
   end
+
 end
