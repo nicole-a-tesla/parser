1843fee3b21bc01019338c1c22a71576e2c0573f
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-01-18 14:32:24 -0500

    collection parser

--- collection_parser.rb
+++ collection_parser.rb
@@ -1,3 +1,48 @@
+require 'Nokogiri'
+require 'pry'
+
 class CollectionParser
+  # input url (?) / text of collection page
+  # each sub-collection has session_title, region, and date
+  # output : navigate through collection, creating new parse_manager for each page_string
+
+  attr_reader :doc, :title, :description, :sessions
+
+  def initialize(page_string)
+    @doc = Nokogiri::HTML(page_string)
+    @title = get_title
+    @description = get_description
+    @sessions = get_sessions
+  end
+
+  def get_title
+    doc.css("h2.collectionHeader")[0].text
+  end
+
+  def get_description
+    description = doc.css("p.collectionDesc")[0].next_element.text
+    description.split(" ").join(" ").strip
+  end
+
+  def get_sessions
+    raw_sessions = doc.css("tbody")[0].css("tr")
+
+    sessions = raw_sessions.map do |session|
+      session_info = session.css("td")
+
+      info = {session_title: session_info[0].text,
+              region: session_info[1].text,
+              date: session_info[2].text,
+              url: session_info[0].children[0].attributes["href"].value}
+      Session.new(info)
+    end
+
+  end
 
 end
+
+# Collection: "Mississippi Prison Recordings 1947 and 1948"
+#   Description: "..."
+#   Sessions: title: "Parchman 12/47",
+#             region: "...",
+#             date: "11-x-19xx"
--- spec/collection_parser_spec.rb
+++ spec/collection_parser_spec.rb
@@ -1,5 +1,45 @@
 require_relative '../collection_parser'
+require_relative '../session'
 
 describe CollectionParser do
+  file = File.open("test/example_collection.html", "rb")
+  page_as_string = file.read
+  let(:collection_parser) { CollectionParser.new(page_as_string) }
+
+  describe "title" do
+    it "gets correct title" do
+      expect(collection_parser.title).to eq "Mississippi Prison Recordings 1947 and 1948"
+    end
+  end
+
+  describe "description" do
+    it "gets the correct description" do
+      expect(collection_parser.description).to eq "The Lomaxes and other collectors of their time (and also decades later) found some of the most powerful vernacular music of the American South in the region’s oppressive and violent prison system. The songs they found there, John and Alan Lomax wrote, “or songs like them were formerly sung all over the South. With the coming of the machines, however, the work gangs were broken up. The songs then followed group labor into its last retreat — the road gang and the penitentiary” (Our Singing Country, 1941). Bruce Jackson, writing about prison song in the 1960s, explains, “Southern agricultural penitentiaries were in many respects replicas of nineteenth-century plantations, where groups of slaves did arduous work by hand, supervised by white men with guns and constant threat of awful physical punishment. It is hardly surprising that the music of plantation culture — the work songs — went to the prisons as well.” The tie-tamping and wood-cutting chants, field hollers, and the occasional blues recorded by Alan Lomax on paper-backed tape at Mississippi’s Parchman Penitentiary in 1947 and 1948 remain among the most vivid documents of this genre of African American song.  "
+    end
+  end
+
+  describe "sessions" do
+    it "has a sessions collection of the correct size" do
+      expect(collection_parser.sessions.size).to eq 2
+    end
+
+    let(:first_session) { collection_parser.sessions[0] }
+
+    it "knows session's title" do
+      expect(first_session.session_title).to eq "Parchman 12/47"
+    end
+
+    it "knows session's region" do
+      expect(first_session.region).to eq "Mississippi, United States"
+    end
+
+    it "knows session's date" do
+      expect(first_session.date).to eq "11-12/1947"
+    end
+
+    it "knows session's url" do
+      expect(first_session.url).to eq "http://research.culturalequity.org/get-audio-ix.do?ix=recording&id=10268&idType=sessionId&sortBy=abc"
+    end
+  end
 
 end
