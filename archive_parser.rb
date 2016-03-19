cc6f74b134e3ad9f4e32f9de374ee4da9071a6bc
nicole-a-tesla <thisisnotnicole@gmail.com>
2016-03-19 12:43:52 -0400

    Add constants and remove init from archive_parser Move urls and collection generation to own method

--- archive_parser.rb
+++ archive_parser.rb
@@ -22,16 +22,19 @@ require_relative "session"
       # foreach row :
         # write row into a new mp3, plus metadata
 class ArchiveParser
+  attr_reader :collection_urls, :collections
 
   URL = "http://research.culturalequity.org/audio-guide.jsp"
   BASE_URL = "http://research.culturalequity.org/"
+  MOTHER_DIR = '/Users/bears8yourface/Documents/lomax2016'
 
-  def initialize
-
+  def build_dir
+    FileUtils.mkdir MOTHER_DIR, mode: 0700
+    return MOTHER_DIR
   end
 
-  def collections
-    @collections ||= []
+  def collection_urls
+    @collection_urls || get_collection_urls
   end
 
   def get_collection_urls
@@ -40,17 +43,15 @@ class ArchiveParser
 
     raw_collections = noko_page.css("td.audioGuideLink")
 
-    # get collection urls
     @collection_urls = raw_collections.map do |collection|
       collection_url = collection.children[1].attributes["href"].value
-      base_url + collection_url
+      BASE_URL + collection_url
     end
   end
 
   def build_collections
-
-    # build collections
-    collections = @collection_urls.map do |collection_url|
+    urls = get_collection_urls
+    @collections = urls.map do |collection_url|
       response = Net::HTTP.get_response(URI.parse(collection_url))
       page_as_string = response.body
 
@@ -58,15 +59,10 @@ class ArchiveParser
     end
   end
 
+  def get_urls_and_build_collections
+    @collection_urls = get_collection_urls
+    @collections = build_collections(@collection_urls)
+    @collections
+  end
+
 end
-
-
-
-
-# collections.each do |collection|
-#   # make a folder
-#   collection.sessions.each do |session|
-#     # make a folder
-#     session.parse
-#   end
-# end
